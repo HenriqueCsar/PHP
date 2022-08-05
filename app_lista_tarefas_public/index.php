e39b75a72aa147b6089d6572a6f57cd3a4983d0e
Henrique Silva <henrique.fibonacci@hotmail.com>
2022-08-05 01:43:46 -0300

    New project - Chat realtime

--- app_lista_tarefas_public/index.php
+++ app_lista_tarefas_public/index.php
@@ -10,7 +10,7 @@
 	</head>
 
 	<body>
-		<nav class="navbar navbar-light bg-light">
+		<nav class="navbar navbar-light bg-light top-fixed">
 			<div class="container">
 				<a class="navbar-brand" href="#">
 					<img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
--- app_lista_tarefas_public/todas_tarefas.php
+++ app_lista_tarefas_public/todas_tarefas.php
@@ -16,7 +16,7 @@ require_once('tarefa_controller.php');
 	</head>
 
 	<body>
-		<nav class="navbar navbar-light bg-light">
+		<nav class="navbar navbar-light bg-light top-fixed">
 			<div class="container">
 				<a class="navbar-brand" href="#">
 					<img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
