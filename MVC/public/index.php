9738b157aa14a7f2d1b90b7e0e69b660ed99d0c2
HenriqueCsar <henrique.fibonacci@hotmail.com>
2022-12-19 02:40:04 -0300

    Update LAB-PHP V20221219

--- MVC/public/index.php
+++ MVC/public/index.php
@@ -4,19 +4,21 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Alura\Cursos\Controller\ListarCursos;
 
-    switch ($_SERVER['PATH_INFO']){
+switch ($_SERVER['PATH_INFO']){
     case '/listar-cursos':
+    
         $controlador = new ListarCursos();
         $controlador->processaRequisicao();
+
+    break;
+
+    case '/novo-curso':
+        require 'novo-curso.php';
         break;
 
-        case '/novo-curso':
-            require 'novo-curso.php';
-            break;
+    case '/save-curso':
+        require '';
 
-        case '/save-curso':
-            require '';
-
-        default:
-            echo 'Error 404';
-    }
+    default:
+        echo 'Error 404';
+}
