b8ebbd69e87ac037970ce18772306cd86ab9c922
HenriqueCsar <henrique.fibonacci@hotmail.com>
2022-12-19 01:49:07 -0300

    Update LAB-PHP V20221219

--- MVC/public/index.php
+++ MVC/public/index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo var_dump($_SERVER);
+
+if($_SERVER['PATH_INFO'] === '/listar-cursos'){
+    require 'listar-cursos.php';
+}
+
+switch ($_SERVER['PATH_INFO']){
+    case '/listar-cursos':
+        require 'listar-cursos.php';
+        break;
+
+    case '/novo-curso':
+        require 'novo_curso.php'
+        break;
+    default:
+        require 'listar-cursos.php';
