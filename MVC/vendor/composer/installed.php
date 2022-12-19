fd8bb4da18bd08c4944a4d5c8922a08d644885b3
HenriqueCsar <henrique.fibonacci@hotmail.com>
2022-12-19 00:51:23 -0300

    Update LAB-PHP V20221219

--- MVC/vendor/composer/installed.php
+++ MVC/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'd8af724011497af9a7ef7f8d7ffd91aec5110489',
+        'reference' => 'bcf470da76cff54dd6eb354882916187d7875913',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'd8af724011497af9a7ef7f8d7ffd91aec5110489',
+            'reference' => 'bcf470da76cff54dd6eb354882916187d7875913',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
