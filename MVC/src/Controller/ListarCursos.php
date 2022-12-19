9fe774e7309d727640b37906a951fdab4476a942
HenriqueCsar <henrique.fibonacci@hotmail.com>
2022-12-19 02:19:41 -0300

    Update LAB-PHP V20221219

---
+++ MVC/src/Controller/ListarCursos.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
+class ListarCursos{
+    function processaRequisicao(){
+        $entityManager = (new EntityManagerCreator())->getEntityManager();
+        $repositorioDeCursos = $entityManager->getRepository(Curso::class);
+        $cursos = $repositorioDeCursos->findAll();
+    }
+}
