a3e04bb38f5ec6d19528fd9033edfa90c8deb7d7
HenriqueCsar <henrique.fibonacci@hotmail.com>
2022-12-19 01:34:50 -0300

    Update LAB-PHP V20221219

--- MVC/public/novo_curso.php
+++ MVC/public/novo_curso.php
@@ -15,13 +15,7 @@
     <a href="novo_curso" class="btn btn-primary">Novo Curso</a>
     <br>
 
-    <ul class="list-group">
-        <?php foreach ($cursos as $curso): ?>
-            <li class="list-group-item">
-                <?= $curso->getDescricao(); ?>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    
 </div>
 </body>
 </html>
